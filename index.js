55d87f150072d1d5fe30f18a6792930e4e2f05a9
KayDrew <kdtshehla@gmail.com>
2024-09-27 10:15:50 +0200

    show users online, disconnect user

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express= require("express");
 const app= express();
 const server= require("http").createServer(app);
-const io= require("socket.io")(server, {cors: {origin:"*"}});
+const io= require("socket.io")(server,
+ {cors: {origin:"*"},
+connectionStateRecovery: {}});
 
 app.set("view engine", "ejs");
 
@@ -14,15 +16,31 @@ server.listen(3000,()=>{
 console.log("running on port 3000");
 });
 
+let users=0;
+
 io.on("connection", (socket)=>{
+	
 console.log("user connected: " + socket.id);
+
+++users;
+io.emit("user count", users);
+console.log(users);
 
 socket.on("message", (data)=>{
 //console.log(data);
-
 //send message to everyone but ourselves 
 socket.broadcast.emit("message", data);
+});
+//handle a disconnect 
+socket.on("disconnect", ()=>{
+	console.log("disconnected");
+--users;
+io.emit("user count", users);
+console.log(users);
+socket.disconnect();
 
 });
+
+
 
 });
